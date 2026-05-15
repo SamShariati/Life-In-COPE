53a8d800a8b7f43d3a4918609e43836a42803fe3
SamShariati <114943247+SamShariati@users.noreply.github.com>
2026-05-15 14:11:57 +0200

    Update GoToShelf.cs

--- Assets/Scripts/AI/Customer/BT Nodes/GoToShelf.cs
+++ Assets/Scripts/AI/Customer/BT Nodes/GoToShelf.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 public class GoToShelf : BTNode
 {
     bool runOnce = false;
+    float distanceTarget;
     public override NodeState Evaluate(CustomerManager agent)
     {
         if (!runOnce)
         {
             runOnce = true;
             Debug.Log("Entered GoToShelf");
+        }
+
+        agent.navigation.speed = agent.walkSpeed;
+        agent.navigation.isStopped = false;
+        agent.navigation.SetDestination(agent.chosenShelfPosition);
+
+        distanceTarget = Vector3.Distance(agent.transform.position, agent.chosenShelfPosition);
+        
+        if (distanceTarget < 0.1f)
+        {
+            return NodeState.SUCCESS;
         }
 
         return NodeState.RUNNING;
