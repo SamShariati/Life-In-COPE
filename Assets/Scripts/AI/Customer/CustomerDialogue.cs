d24c14b0885ab80579d19780235a82f270c6b056
SamShariati <114943247+SamShariati@users.noreply.github.com>
2026-07-02 22:34:45 +0200

    d

--- Assets/Scripts/AI/Customer/CustomerDialogue.cs
+++ Assets/Scripts/AI/Customer/CustomerDialogue.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class CustomerDialogue : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public CustomerDialogue (CustomerManager agent)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
     }
 }
